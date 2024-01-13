821a86e0e16bfc0d46aab216b4bc441f0c33b161
005abhi <abhinandan111003@gmail.com>
2024-01-13 17:13:57 +0530

    Fresh Start clean routings.    redirecting to login isn't working

--- src/app/SkillHive/page.tsx
+++ src/app/SkillHive/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+export const page = () => {
   return (
     <main style={{ backgroundColor: "lightblue" }}>
 
@@ -36,19 +36,7 @@ export default function Home() {
 
             </div>
 
-            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
 
-              <div className="absolute top-39 right-10 mr-4 mt-4" style={{ color: "white" }}>
-                <Link href="/Login">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 448 512" fill='white'>
-                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                  </svg>
-                </Link>
-
-
-              </div>
-
-            </nav>
             {/* Remove the button at the end */}
           </div>
         </header>
@@ -107,4 +95,4 @@ export default function Home() {
   )
 }
 
-
+export default page;
